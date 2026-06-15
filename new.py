f129aa9193ac050e5f9fc611eab0a5b0a85714c6
kannan-ms <kannanmayilsamy04@gmail.com>
2026-06-15 21:32:32 +0530

    changes are commited

--- new.py
+++ new.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By 
+
 browser = webdriver.Chrome()
-browser.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-print("open browser")
-title = browser.title
-print("title: " + title)
+
+driver.find_element_by_id()
